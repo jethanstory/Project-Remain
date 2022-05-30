38997e0afcf036cff5d8337cff5d3f4f06b124d0
jethanstory <32407746+jethanstory@users.noreply.github.com>
2022-05-29 19:11:35 -0700

    Added flare, tags, basic pickup script, basic flare throw script, lights and particle effects

---
+++ Project Remain/Assets/Scripts/ThrowingObject.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+//this script is used for throwing the grenade and destrouing it after some delay
+
+public class ThrowingObject : MonoBehaviour
+
+{
+
+    public Transform spawnPoint;
+    public GameObject flare;
+
+    float range = 15f;
+
+    public Rigidbody rb;
+
+    public ParticleSystem part;
+
+    public Light light;
+    public Light light2;
+    public Light light3;
+
+    // Update is called once per frame
+
+    public GameObject[] lights;
+    private bool lightEnabled;
+
+
+
+    void Start () {
+        //light = GetComponent<Light> ();
+        //light2 = GetComponent<Light> ();
+        //light3 = GetComponent<Light> ();
+    }
+
+    void FixedUpdate()
+
+    {
+
+        if (Input.GetMouseButtonDown(0))
+
+        {
+            //Destroy(flare);
+
+            //rb.AddForce(new Vector3(0,15,15), ForceMode.Impulse);// used to apply force 
+
+            //Invoke("delay", 4f);//it is used to create delay in destroying the game object 
+            Launch();
+
+        }
+
+    }
+
+    public void delay() // whenever this function is called the object gets destroyed
+
+    {
+
+        //Destroy(gameObject);
+
+
+    }
+    private void Launch()
+    {
+        flare.transform.SetParent(null);
+        GameObject flareInstance = Instantiate(flare, spawnPoint.position, spawnPoint.rotation);
+        flareInstance.GetComponent<Rigidbody>().AddForce(spawnPoint.forward * range, ForceMode.Impulse);
+        flareInstance.GetComponent <ParticleSystem>().Play ();
+        ParticleSystem.EmissionModule em = GetComponent<ParticleSystem>().emission;
+        //light = flareInstance.GetComponent <Light>();
+        em.enabled = true;
+
+        lightEnabled = !lightEnabled;
+
+        foreach (var light in lights)
+        {
+            light.SetActive(lightEnabled);
+        }
+        //light.enabled = true;
+        //light2.enabled = true;
+        //light3.enabled = true;
+
+
+    }
+
+}
