5df00762579bed2c09acc8d29630d21e5ffd33c2
jethanstory <32407746+jethanstory@users.noreply.github.com>
2023-01-18 14:45:46 -0700

    Added timescale to fix pause menu

--- Project Remain/Assets/Scripts/MouseLook.cs
+++ Project Remain/Assets/Scripts/MouseLook.cs
@@ -16,6 +16,7 @@ public class MouseLook : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
