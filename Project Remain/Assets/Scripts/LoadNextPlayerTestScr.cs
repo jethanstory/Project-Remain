08e1ddde67e9291c3346d662d2e4875565392672
jethanstory <32407746+jethanstory@users.noreply.github.com>
2023-05-29 07:51:05 -0700

    Added player switch experiment

---
+++ Project Remain/Assets/Scripts/LoadNextPlayerTestScr.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadNextPlayerTestScr : MonoBehaviour
+{
+    public GameObject fpsPlayer;
+    public GameObject fpsPlayerRanger;
+    public GameObject fpsPlayerObjects;
+    public GameObject gameOverCanvas;
+    public GameObject rangerIntroCanvas;
+    //public GameObject wanderEnemy;
+    //public GameObject longOneEnemy;
+    public float secondsCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        //Destroy(fpsPlayer);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkPlayer();
+    }
+
+    void checkPlayer()
+    {
+        if (fpsPlayer.GetComponent<PlayerCollision>().hasCollided)
+        {
+            secondsCount += Time.deltaTime;
+            gameOverCanvas.SetActive(true);
+            fpsPlayerObjects.SetActive(true);
+            fpsPlayerObjects.transform.position = fpsPlayer.transform.position;
+            if (secondsCount > 2) 
+            {
+                rangerIntroCanvas.SetActive(true);
+                gameOverCanvas.SetActive(false);
+                fpsPlayer.SetActive(false);
+                //Destroy(fpsPlayer);
+                fpsPlayerRanger.SetActive(true);
+                if (secondsCount > 3) 
+                {
+                    
+                    rangerIntroCanvas.SetActive(false);
+                    //Destroy(wanderEnemy);
+                    //Destroy(longOneEnemy);
+                }
+            }
+            
+            
+        }
+    }
+}
--- Project Remain/Assets/Scripts/PlayerCollision.cs
+++ Project Remain/Assets/Scripts/PlayerCollision.cs
@@ -6,19 +6,12 @@ using UnityEngine.SceneManagement;
 
 public class PlayerCollision : MonoBehaviour
 {
+    public bool hasCollided;
+    GameObject ObjectIwantToDestroy;
     // Start is called before the first frame update
-    void OnCollisionEnter (UnityEngine.Collision collisionInfo) 
+    void Start()
     {
-        //if (collisionInfo.collider.name == "Enemy" || collisionInfo.collider.name == "WanderingEnemy" || collisionInfo.collider.name == "flyingEnemy" || collisionInfo.collider.name == "longOne" )
-        if (collisionInfo.collider.tag == "Enemy")
-        {
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
-            SceneManager.LoadScene("GameOver");
-            Debug.Log("HIT");
-            
-        }
-        
+        //gameOverCanvas.SetActive(false);
     }
 
     // Update is called once per frame
@@ -26,6 +19,39 @@ public class PlayerCollision : MonoBehaviour
     {
         
     }
-}
+
 
 /*|| collisionInfo.collider.name == "longOne" || collisionInfo.collider.name == "watcherEnemy" */
+
+
+// void OnCollisionEnter (UnityEngine.Collision collisionInfo) 
+//     {
+//         //if (collisionInfo.collider.name == "Enemy" || collisionInfo.collider.name == "WanderingEnemy" || collisionInfo.collider.name == "flyingEnemy" || collisionInfo.collider.name == "longOne" )
+//         if (collisionInfo.collider.tag == "Enemy")
+//         {
+//             hasCollided = true;
+//             //Cursor.lockState = CursorLockMode.None;
+//             //Cursor.visible = true;
+            
+//             //SceneManager.LoadScene("GameOver");
+//             Debug.Log("HIT");
+            
+            
+//         }
+        
+//     }
+
+private void OnTriggerEnter(Collider other) // to see when the player enters the collider
+    {
+        if(other.gameObject.tag == "Enemy") //on the object you want to pick up set the tag to be anything, in this case "object"
+        {
+            // canpickup = true;  //set the pick up bool to true
+            // ObjectIwantToPickUp = other.gameObject; //set the gameobject you collided with to one you can reference
+
+            Debug.Log("HIT");
+            hasCollided = true;
+            //ObjectIwantToDestroy = other.gameObject; //set the gameobject you collided with to one you can reference
+            //Destroy(ObjectIwantToDestroy);
+        }
+    }  
+}
