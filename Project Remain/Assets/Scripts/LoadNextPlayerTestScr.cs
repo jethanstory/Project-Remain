cabfbe04243f82ae87c22d4621de16e0457b1f69
jethanstory <32407746+jethanstory@users.noreply.github.com>
2023-11-10 00:09:32 -0700

    Added some fixes to player change test script

--- Project Remain/Assets/Scripts/LoadNextPlayerTestScr.cs
+++ Project Remain/Assets/Scripts/LoadNextPlayerTestScr.cs
@@ -9,13 +9,16 @@ public class LoadNextPlayerTestScr : MonoBehaviour
     public GameObject fpsPlayerObjects;
     public GameObject gameOverCanvas;
     public GameObject rangerIntroCanvas;
-    //public GameObject wanderEnemy;
-    //public GameObject longOneEnemy;
+    public GameObject wanderEnemy;
+    public GameObject longOneEnemy;
+
+    public GameObject wanderEnemyNext;
+    public GameObject longOneEnemyNext;
     public float secondsCount;
     // Start is called before the first frame update
     void Start()
     {
-        
+
         //Destroy(fpsPlayer);
 
     }
@@ -34,23 +37,27 @@ public class LoadNextPlayerTestScr : MonoBehaviour
             gameOverCanvas.SetActive(true);
             fpsPlayerObjects.SetActive(true);
             fpsPlayerObjects.transform.position = fpsPlayer.transform.position;
-            if (secondsCount > 2) 
+            if (secondsCount > 2)
             {
                 rangerIntroCanvas.SetActive(true);
                 gameOverCanvas.SetActive(false);
                 fpsPlayer.SetActive(false);
                 //Destroy(fpsPlayer);
                 fpsPlayerRanger.SetActive(true);
-                if (secondsCount > 3) 
+                wanderEnemy.SetActive(false);
+                wanderEnemyNext.SetActive(true);
+                longOneEnemy.SetActive(false);
+                longOneEnemyNext.SetActive(true);
+                if (secondsCount > 3)
                 {
-                    
+
                     rangerIntroCanvas.SetActive(false);
                     //Destroy(wanderEnemy);
                     //Destroy(longOneEnemy);
                 }
             }
-            
-            
+
+
         }
     }
 }
--- Project Remain/Assets/Scripts/PlayerCollision.cs
+++ Project Remain/Assets/Scripts/PlayerCollision.cs
@@ -17,41 +17,41 @@ public class PlayerCollision : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
 
-/*|| collisionInfo.collider.name == "longOne" || collisionInfo.collider.name == "watcherEnemy" */
+    /*|| collisionInfo.collider.name == "longOne" || collisionInfo.collider.name == "watcherEnemy" */
 
 
-// void OnCollisionEnter (UnityEngine.Collision collisionInfo) 
-//     {
-//         //if (collisionInfo.collider.name == "Enemy" || collisionInfo.collider.name == "WanderingEnemy" || collisionInfo.collider.name == "flyingEnemy" || collisionInfo.collider.name == "longOne" )
-//         if (collisionInfo.collider.tag == "Enemy")
-//         {
-//             hasCollided = true;
-//             //Cursor.lockState = CursorLockMode.None;
-//             //Cursor.visible = true;
-            
-//             //SceneManager.LoadScene("GameOver");
-//             Debug.Log("HIT");
-            
-            
-//         }
-        
-//     }
+    // void OnCollisionEnter (UnityEngine.Collision collisionInfo) 
+    //     {
+    //         //if (collisionInfo.collider.name == "Enemy" || collisionInfo.collider.name == "WanderingEnemy" || collisionInfo.collider.name == "flyingEnemy" || collisionInfo.collider.name == "longOne" )
+    //         if (collisionInfo.collider.tag == "Enemy")
+    //         {
+    //             hasCollided = true;
+    //             //Cursor.lockState = CursorLockMode.None;
+    //             //Cursor.visible = true;
 
-private void OnTriggerEnter(Collider other) // to see when the player enters the collider
+    //             //SceneManager.LoadScene("GameOver");
+    //             Debug.Log("HIT");
+
+
+    //         }
+
+    //     }
+
+    private void OnTriggerEnter(Collider other) // to see when the player enters the collider
     {
-        if(other.gameObject.tag == "Enemy") //on the object you want to pick up set the tag to be anything, in this case "object"
+        if (other.gameObject.tag == "Enemy") //on the object you want to pick up set the tag to be anything, in this case "object"
         {
             // canpickup = true;  //set the pick up bool to true
             // ObjectIwantToPickUp = other.gameObject; //set the gameobject you collided with to one you can reference
 
             Debug.Log("HIT");
             hasCollided = true;
-            ObjectIwantToDestroy = other.gameObject; //set the gameobject you collided with to one you can reference
-            Destroy(ObjectIwantToDestroy);
+            // ObjectIwantToDestroy = other.gameObject; //set the gameobject you collided with to one you can reference
+            //Destroy(ObjectIwantToDestroy);
         }
-    }  
+    }
 }
